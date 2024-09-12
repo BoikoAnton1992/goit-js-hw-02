75ded3356d5ef6b6eb9d1eefeed6b9e5554767ca
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-09-12 12:28:00 +0300

    Add new task

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,14 @@
-
 // quantity — кількість замовлених дроїдів
 // pricePerDroid — ціна одного дроїда
 // customerCredits — сума коштів на рахунку клієнта
 
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalPrice = quantity * pricePerDroid;
-    if (totalPrice > customerCredits) {
-        return "Insufficient funds!"
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    }
+  let totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customerCredits) {
+    return 'Insufficient funds!';
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
@@ -17,3 +16,94 @@ console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 300
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+function checkAccess(age) {
+  if (age < 18) {
+    return 'Access denied: You must be at least 18 years old.';
+  } else {
+    return 'Access granted: Welcome!';
+  }
+}
+
+console.log(checkAccess(16)); // "Access denied: You must be at least 18 years old."
+console.log(checkAccess(18)); // "Access granted: Welcome!"
+console.log(checkAccess(21)); // "Access granted: Welcome!"
+
+function isEven(number) {
+  if (number % 2 !== 0) {
+    return 'Odd';
+  } else {
+    return 'Even';
+  }
+}
+
+console.log(isEven(4)); // "Even"
+console.log(isEven(7)); // "Odd"
+console.log(isEven(0)); // "Even"
+
+function chooseSubscription(isStudent, wantsPremium) {
+  if (isStudent) {
+    return 'Basic Student Package';
+  } else if (wantsPremium) {
+    return 'Premium Package';
+  } else {
+    return 'Basic Package';
+  }
+}
+
+console.log(chooseSubscription(true, true)); // "Basic Student Package"
+console.log(chooseSubscription(false, true)); // "Premium Package"
+console.log(chooseSubscription(false, false)); // "Basic Package"
+
+function isEligibleForDiscount(totalAmount, isMember) {
+  if (totalAmount >= 100) {
+    return 'Eligible for discount';
+  } else if (totalAmount < 100 && isMember === true) {
+    return 'Eligible for discount';
+  } else {
+    return 'Not eligible for discount';
+  }
+}
+
+console.log(isEligibleForDiscount(120, false)); // "Eligible for discount"
+console.log(isEligibleForDiscount(50, true)); // "Eligible for discount"
+console.log(isEligibleForDiscount(50, false)); // "Not eligible for discount"
+
+function isAvailable(productInStock, isDiscontinued) {
+  if (productInStock === true && !isDiscontinued) {
+    return 'Available';
+  } else {
+    return 'Not available';
+  }
+}
+
+console.log(isAvailable(true, false)); // "Available"
+console.log(isAvailable(false, true)); // "Not available"
+console.log(isAvailable(false, false)); // "Not available"
+
+// Завдання 3: Перевірка доступу до ресурсу
+// Напиши функцію hasAccess(isAdmin, hasPermission, isBanned), яка приймає три параметри:
+
+// isAdmin — булеве значення, яке вказує, чи є користувач адміністратором.
+// hasPermission — булеве значення, яке вказує, чи є у користувача дозвіл на доступ.
+// isBanned — булеве значення, яке вказує, чи є користувач заблокованим.
+// Функція повинна повертати рядок:
+
+// "Access granted" — якщо користувач є адміністратором або має дозвіл, і він не заблокований.
+// "Access denied" — в інших випадках.
+
+function hasAccess(isAdmin, hasPermission, isBanned) {
+  if (isBanned === true) {
+    return 'Access denied';
+  } else if (isAdmin === true) {
+    return 'Access granted';
+  } else if (hasPermission === true) {
+    return 'Access granted';
+  } else {
+    return 'Access denied';
+  }
+}
+
+console.log(hasAccess(true, false, false)); // "Access granted"
+console.log(hasAccess(false, true, false)); // "Access granted"
+console.log(hasAccess(false, true, true)); // "Access denied"
--- js/task-4.js
+++ js/task-4.js
@@ -1,33 +1,76 @@
-
-
 function getShippingCost(country) {
-    let price;
-    let message;
+  let price;
+  let message;
 
-    switch (country) {
-  case "China":
-    price = 100
-    break;
-      case "Chile":
-    price = 250
-        break;
-      case "Australia":
-    price = 170
-        break;
-       case "Jamaica":
-    price = 120
-        break;
-        default:
-            return "Sorry, there is no delivery to your country";
-    }
-    message = `Shipping to ${country} will cost ${price} credits`
-    return message;
+  switch (country) {
+    case 'China':
+      price = 100;
+      break;
+    case 'Chile':
+      price = 250;
+      break;
+    case 'Australia':
+      price = 170;
+      break;
+    case 'Jamaica':
+      price = 120;
+      break;
+    default:
+      return 'Sorry, there is no delivery to your country';
+  }
+  message = `Shipping to ${country} will cost ${price} credits`;
+  return message;
 }
 
+console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
 
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+// function checkNumberType(number) {
+//   return number > 0 ? 'Positive' : number < 0 ? 'Negative' : 'Zero';
+// }
+
+// function getAccessMessage(isUser, hasPermission) {
+//   return isUser && hasPermission ? 'Access granted' : 'Access denied';
+// }
+
+// function calculateDiscount(isVIP) {
+//   return isVIP ? '20% discount' : '10% discount';
+// }
+
+// Завдання 1: Перевірка температури
+// Створи функцію checkTemperature(temp), яка приймає один параметр
+// temp(число) і повертає рядок "Hot", якщо температура більша або дорівнює 30, або "Cold", якщо менша за 30.
+
+// function checkTemperature(temp) {
+//   return temp >= 30 ? 'Hot' : 'Cold';
+// }
+
+// console.log(checkTemperature(35)); // "Hot"
+// console.log(checkTemperature(20)); // "Cold"
+
+// Завдання 2: Визначення більшого числа
+// Створи функцію getMaxNumber(a, b), яка приймає два числа a і b і
+// повертає більше з них.Якщо числа рівні, функція має повернути рядок "Numbers are equal".
+
+// function getMaxNumber(a, b) {
+//   return a > b ? a : a < b ? b : 'Numbers are equal';
+// }
+
+// console.log(getMaxNumber(10, 20)); // 20
+// console.log(getMaxNumber(30, 15)); // 30
+// console.log(getMaxNumber(5, 5)); // "Numbers are equal"
+
+// Завдання 3: Перевірка парності числа
+// Створи функцію isOddOrEven(number), яка приймає один параметр number(число) і
+// повертає "Even", якщо число парне, або "Odd", якщо непарне.
+
+// function isOddOrEven(number) {
+//   return number % 2 ? 'Оdd' : 'Even';
+// }
+
+// console.log(isOddOrEven(4)); // "Even"
+// console.log(isOddOrEven(7)); // "Odd"
